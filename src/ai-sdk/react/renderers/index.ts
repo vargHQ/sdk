d216827118deedc2d70b8574a1daa7c4e8c48495
Aleksey Bykhun <theisease@gmail.com>
2026-01-14 19:44:24 -0800

    refactor(react): split render.ts into modular renderer files

---
+++ src/ai-sdk/react/renderers/index.ts
@@ -0,0 +1,7 @@
+export { renderAnimate } from "./animate";
+export { renderClip } from "./clip";
+export type { RenderContext } from "./context";
+export { renderImage } from "./image";
+export { renderTitle } from "./title";
+export { computeCacheKey, getTextContent } from "./utils";
+export { renderVideo } from "./video";
