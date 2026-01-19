d65bf713e4c3839fbda94fa6a7edc99e22de5c23
Aleksey Bykhun <theisease@gmail.com>
2026-01-18 19:53:36 -0800

    chore: remove unused v2 sdk

--- src/v2/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./elevenlabs";
-export * from "./fal-image";
-export * from "./fal-sync";
-export * from "./fal-transcription";
-export * from "./fal-video";
-export * from "./groq";
-export * from "./replicate";
