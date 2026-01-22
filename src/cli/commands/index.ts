321ccfbb7e1c6cdf60850ff24ff9d1263d4885b8
Aleksey Bykhun <theisease@gmail.com>
2026-01-21 21:14:11 -0800

    fix: add react as peer dep and rename non-jsx commands to .ts

--- src/cli/commands/index.ts
+++ src/cli/commands/index.ts
@@ -1,7 +1,7 @@
 export { findCmd, showFindHelp } from "./find.tsx";
 export { helpCmd, showHelp } from "./help.tsx";
 export { listCmd, showListHelp } from "./list.tsx";
-export { renderCmd } from "./render.tsx";
+export { renderCmd } from "./render.ts";
 export { runCmd, showRunHelp, showTargetHelp } from "./run.tsx";
-export { studioCmd } from "./studio.tsx";
+export { studioCmd } from "./studio.ts";
 export { showWhichHelp, whichCmd } from "./which.tsx";
